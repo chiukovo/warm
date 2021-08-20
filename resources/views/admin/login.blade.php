e081b25af9b132938d1b57b505e10044d71a357c
chiukovo <chiuko@celloes.com>
2021-08-20 15:54:39 +0800

    fix

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -10,7 +10,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>{{ env('APP_NAME') }}</title>
+    <title>{{ env('APP_NAME') }}管理者後台</title>
 
     <!-- Custom fonts for this template-->
     <link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
