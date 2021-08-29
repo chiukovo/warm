715c9b3bad32e11f40754ec7aebbd6cb8cb4bfdf
chiuko <chiuko@1,1>
2021-08-29 22:35:38 +0800

    add

--- resources/views/admin/layouts/left-sidebar.blade.php
+++ resources/views/admin/layouts/left-sidebar.blade.php
@@ -9,13 +9,13 @@
                     <a href="/admin/index/banner/list" class="sidebar-link waves-dark"><i class="mdi mdi-file-image"></i><span class="hide-menu">首頁輪播圖</span></a>
                 </li>
                 <li class="sidebar-item">
-                    <a href="/admin/about/list" class="sidebar-link waves-dark"><i class="mdi mdi-file-image"></i><span class="hide-menu">關於我們</span></a>
+                    <a href="/admin/about/list" class="sidebar-link waves-dark"><i class="mdi mdi-account"></i><span class="hide-menu">關於我們</span></a>
                 </li>
                 <li class="sidebar-item">
-                    <a href="/admin/types/list" class="sidebar-link waves-dark"><i class="mdi mdi-file-image"></i><span class="hide-menu">產品分類</span></a>
+                    <a href="/admin/types/list" class="sidebar-link waves-dark"><i class="mdi mdi-format-list-bulleted-type"></i><span class="hide-menu">產品分類</span></a>
                 </li>
                 <li class="sidebar-item">
-                    <a href="/admin/products/list" class="sidebar-link waves-dark"><i class="mdi mdi-file-image"></i><span class="hide-menu">產品列表</span></a>
+                    <a href="/admin/products/list" class="sidebar-link waves-dark"><i class="mdi mdi-codepen"></i><span class="hide-menu">產品列表</span></a>
                 </li>
                 <li class="sidebar-item">
                     <a href="/admin/filemanager-page" class="sidebar-link waves-dark"><i class="mdi mdi-file-image"></i><span class="hide-menu">圖片管理</span></a>
